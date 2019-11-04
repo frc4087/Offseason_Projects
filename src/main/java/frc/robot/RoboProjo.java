b1aac6e94f37d35f63dd3038def17662887d4304
Alex <amazier22@bfhsla.org>
2019-11-04 17:05:03 -0600

    Add files via upload

--- src/main/java/frc/robot/RoboProjo.java
+++ src/main/java/frc/robot/RoboProjo.java
@@ -2,17 +2,8 @@
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
-
-
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
-
-  //// CREATING BUTTONS
+/* the project.           
+//// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
   //// joystick.
   // You create one by telling it which joystick it's on and which button
@@ -38,40 +29,10 @@
 
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+  // button.whenReleased(new ExampleCommand());       
+  /**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */                                             
+/*----------------------------------------------------------------------------*/
 
-package frc.robot;
-
-import edu.wpi.first.wpilibj.XboxController;
-
-public class OI {
-  
-
-  public static final double DEADZONE = 0.1;
-
-  public final XboxController XboxDrive = new XboxController(0);
-
-  //X axis Left Stick
-  public double getXboxDriveXL() {
-    double raw = XboxDrive.getRawAxis(0);
-    return Math.abs(raw) < DEADZONE ? 0.0 : raw;
-  }
-
-  //Y axis Left Stick
-  public double getXboxDriveYL() {
-  double raw = XboxDrive.getRawAxis(1);
-   return Math.abs(raw) < DEADZONE ? 0.0 : raw;
-  }
-
-  //X axis Right Stick
-  public double getXboxDriveXR() {
-   double raw = XboxDrive.getRawAxis(0);
-   return Math.abs(raw) < DEADZONE ? 0.0 : raw;
-  }
-
-  //Y axis Right stick
-  public double getXboxDriveYR() {
-    double raw = XboxDrive.getRawAxis(5);
-    return Math.abs(raw) < DEADZONE ? 0.0 : raw;
-   }
-}
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,19 +2,9 @@
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/* the project.                        
 
-package frc.robot;
-
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
-public class RobotMap {
-  // For example to map the left and right motors, you could define the
+ // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
   // public static int rightMotor = 2;
@@ -22,5 +12,32 @@ public class RobotMap {
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+  // public static int rangefinderModule = 1;*/
+  /**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+
+public enum RobotMap {
+ 
+  //Drivebase mappings
+  FL_DRIVE(1),
+  FR_DRIVE(2),
+  ML_DRIVE(0),
+  BL_DRIVE(1),
+  MR_DRIVE(2),
+  BR_DRIVE(3);
+
+
+  public final int value;
+
+  RobotMap(int value){
+  this.value = value; 
+}
 }
